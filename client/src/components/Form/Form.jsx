f5c4ecb21c8f2abfe755dcb33fc9d8fb147efb96
Arnold Hilberink <a.hilberink42@gmail.com>
2023-02-26 02:02:54 +0100

    init commit

---
+++ client/src/components/Form/Form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import useStyles from "./styles";
+
+const Form = ({ children, onSubmit }) => {
+  const classes = useStyles();
+  return (
+    <div>Form</div>
+  );
+};
+
+export default Form;
