99abadf9ab7096b63f061a0f156cb114cc4ce07d
Arnold Hilberink <a.hilberink42@gmail.com>
2023-02-26 20:28:55 +0100

    reducer posts done

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux"; // useDispatch is a hook that returns the dispatch method from the Redux store, which is used to dispatch actions and trigger state changes to the store.
+
+import { getPosts } from "./actions/posts"; // getPosts is an action creator that returns an action object with a type and payload. The type is a string that describes the action that occurred. The payload is the data that is being sent to the reducer. The reducer will use the type to determine what to do with the payload. The reducer will then return a new state object that will be used to update the store.
 import memories from "./images/memories.png";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
@@ -7,6 +10,12 @@ import useStyles from "./styles";
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts()); // dispatch is a method that takes an action as an argument and sends it to the reducer.  
+    }, [dispatch]);
+
     return (
         <>
         <Container maxWidth="lg">
